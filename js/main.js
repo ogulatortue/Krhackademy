0b238dfa416b983501f12850eb8c0221765d6b43
berlioro <robbie07130@gmail.com>
2025-04-23 15:58:19 +0200

    centered mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-document.getElementById("mobile-menu").addEventListener("click", function() {
-    const menu = document.querySelector(".header-right");
-    menu.classList.toggle("active"); // Active ou désactive la visibilité du menu
+// On attend que le DOM soit entièrement chargé
+document.addEventListener('DOMContentLoaded', function () {
+    // Récupère le bouton hamburger et le menu mobile
+    const mobileMenuButton = document.getElementById('mobile-menu');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    // Ajoute un écouteur d'événement au clic sur le bouton hamburger
+    mobileMenuButton.addEventListener('click', function () {
+        // Basculer la classe 'active' pour afficher ou cacher le menu
+        mobileNav.classList.toggle('active');
+    });
 });

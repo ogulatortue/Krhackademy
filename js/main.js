0055e59988baddc00f6984d3e351e78e6f67e031
ogulatortue <bruch.hugo@gmail.com>
2025-04-23 20:45:33 +0200

    mobile+header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 
 $(document).ready(function(){
+
+
     $('.fa-bars').click(function(){
         $('.nav_bar').toggleClass('open');
         $(this).toggleClass('fa-times');
     });
+
+    $(window).on('scroll load',function(){
+        $('.nav_bar').removeClass('open');
+        $('.fa-bars').removeClass('fa-times');
+
+        if($(window).scrollTop() > 20){
+            $('header').addClass('header-active')
+        }else{
+            $('header').removeClass('header-active')
+        }
+    });
 });
+

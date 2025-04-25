ab41fdeed950e6f055c8564028c40e8f0edb8866
berlioro <robbie07130@gmail.com>
2025-04-25 09:30:24 +0200

    add section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.fa-bars');
+    const navBar = document.querySelector('.nav_bar');
+    const header = document.querySelector('header');
+    const projects = document.getElementById("Projets");
 
-$(document).ready(function(){
-    
-    $('.fa-bars').click(function(){
-        $('.nav_bar').toggleClass('open');
-        $(this).toggleClass('fa-times');
+    menuIcon.addEventListener('click', function () {
+        navBar.classList.toggle('open');
+        menuIcon.classList.toggle('fa-times');
     });
 
-    $(window).on('scroll load',function(){
-        $('.nav_bar').removeClass('open');
-        $('.fa-bars').removeClass('fa-times');
+    projects.addEventListener('click', function () {
+        navBar.classList.remove('open');
+        menuIcon.classList.toggle('fa-times');
+    });
 
-        if($(window).scrollTop() > 20){
-            $('header').addClass('header-active')
-        }else{
-            $('header').removeClass('header-active')
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('load', handleScroll);
+
+    function handleScroll() {
+        navBar.classList.remove('open');
+        menuIcon.classList.remove('fa-times');
+
+        if (window.scrollY > 20) {
+            header.classList.add('header-active');
+        } else {
+            header.classList.remove('header-active');
         }
-    });
+    }
 });
-

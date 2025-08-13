d7444069cbf9b2f32185f1b35ac0228ffafd3ba7
Kolo <bruch.hugo@gmail.com>
2025-08-13 16:01:02 +0200

    fix sécurité

--- back-end/db_connect.php
+++ back-end/db_connect.php
@@ -1,11 +1,20 @@
 <?php
 
-$host = '127.0.0.1'; // or 'localhost'
-$db_name = 'krhackademy';
-$username = 'root';
-$password = 'WEyP3-6jnAEU/aqz';
-$charset = 'utf8mb4';
+// 1. Charge l'autoloader de Composer pour utiliser les librairies
+require_once __DIR__ . '/../vendor/autoload.php';
 
+// 2. Charge les variables du fichier .env dans l'environnement
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+// 3. Utilise les variables sécurisées de l'environnement ($_ENV)
+$host = $_ENV['DB_HOST'];
+$db_name = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS']; // <- Le mot de passe est maintenant lu ici
+$charset = $_ENV['DB_CHARSET'];
+
+// Le reste de votre code de connexion reste le même
 $dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,

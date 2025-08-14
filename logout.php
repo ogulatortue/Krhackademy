f63ecc0f84e9b825a2cd46e2804a9a7e5e48c358
Kolo <bruch.hugo@gmail.com>
2025-08-14 09:51:20 +0200

    fix déconnexion

--- logout.php
+++ logout.php
@@ -1,14 +1,18 @@
 <?php
-// logout.php
 session_start();
 
-// Détruit toutes les variables de session
 $_SESSION = array();
 
-// Détruit la session
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
 session_destroy();
 
-// Redirige vers la page d'accueil
-header("location: index.html");
+header("location: index.php");
 exit;
 ?>

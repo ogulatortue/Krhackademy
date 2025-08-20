0e8e45b743cb799157184c6353b909f86421b50c
Kolo <bruch.hugo@gmail.com>
2025-08-20 18:43:32 +0200

    footer

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -2,7 +2,12 @@
         <a href="https://krhacken.org" target="_blank" rel="noopener noreferrer"> <img src="./images/logo.webp" alt="Logo du club Krhacken"> </a>
         <a href="https://esisar.grenoble-inp.fr" target="_blank" rel="noopener noreferrer"> <img src="./images/logo_esisar.webp" alt="Logo de Grenoble-INP Esisar"> </a>
         <p class="footer-legal">
-            Ce site a été conçu et développé par Hugo BRUCHON. Le club Kr[HACK]en est autorisé à utiliser et à publier ce site à des fins non commerciales. Tous les droits d'auteur, de reproduction et de propriété intellectuelle du code, du design et des contenus originaux restent la propriété exclusive d'Hugo BRUCHON. Toute reproduction totale ou partielle sans autorisation expresse est formellement interdite.
+        <p>
+Ce site a été conçu et développé par Hugo BRUCHON. Le club Kr[HACK]en est autorisé à l'utiliser, le modifier et à le publier pour un usage non commercial.
+
+Les droits d'auteur, de reproduction et de propriété intellectuelle du code appartiennent à Hugo BRUCHON. Toute reproduction, même partielle, sans son accord est interdite.
+
+Le contenu créé par d'autres contributeurs et publié sur ce site reste la propriété de leurs auteurs respectifs. 
         </p>
     </footer>
 

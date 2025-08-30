8f15403a24cae3ef76c2ba5bcce7be9fef6d74be
Kolo <bruch.hugo@gmail.com>
2025-08-30 14:29:24 +0200

    fix oui encore un

--- src/controllers/logout-logic.php
+++ src/controllers/logout-logic.php
@@ -2,23 +2,16 @@
 
 session_start();
 
-$_SESSION = [];
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-session_destroy();
-
-session_start();
 $_SESSION['flash_message'] = [
     'type' => 'success',
     'title' => 'Déconnexion',
     'message' => 'Vous avez été déconnecté avec succès.'
 ];
+
+unset($_SESSION['user_id']);
+unset($_SESSION['username']);
+
+session_regenerate_id(true);
 
 header('Location: /');
 exit();
